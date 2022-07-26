e1d56f50f7a70724b9f6342d30aeb9b8515820a2
guguducken <1173781832@qq.com>
2022-07-26 22:32:28 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { graphql } = require("@octokit/graphql");
+
+// const accessToken = process.env['GITHUB_TOKEN'];
+const accessToken = core.getInput('github-token', { required: true });
+// const accessToken = process.env['GITHUB_TOKEN'];
+
+
+async function run() {
+    try {
+
+        const context = github.context;
+        const pr = context.payload.pull_request;
+        const num = pr.number       //获取PR的序号
+        core.info(JSON.stringify(num));
+        core.info(JSON.stringify(accessToken));
+
+        //获取PR的paths
+        const graphqlWithAuth = graphql.defaults({
+            headers: {
+                authorization: `token ` + accessToken,
+            },
+        });
+        const { pr_paths } = await graphqlWithAuth(`
+            {
+                repository(name: "matrixone", owner: "matrixorigin") {
+                    pullRequest(number: 4174) {
+                        files(first: 100) {
+                            edges {
+                                node {
+                                    path
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        `);
+        core.info("---------------------------------------------------------");
+        core.info(JSON.stringify(pr_paths));
+        core.info("---------------------------------------------------------");
+    } catch (err) {
+        core.setFailed(err.message);
+    }
+}
+
+run();
